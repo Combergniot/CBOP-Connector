f7ed029d7aab54b4c93c610abb07fa8ccd1aeca7
Combergniot <marcin.wernerowicz@gmail.com>
2018-12-11 10:21:53 +0100

    Corrections and comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * App send single request in XML to CBOP server after 10 hours waiting.
+ * The server sends json files with data from 5:00 pm to 7:00 am
+ *
+ */
 public class Main extends Settings {
 
     private static final ScheduledExecutorService scheduler =
@@ -12,7 +17,7 @@ public class Main extends Settings {
     public static void main(String[] args) {
         Methods methods = new Methods();
 
-          System.out.println(LocalDateTime.now().format(formatter) +
+        System.out.println(LocalDateTime.now().format(formatter) +
                 ": Starting ten-hour countdown now...");
 
         Runnable runnable = () -> {
@@ -20,11 +25,8 @@ public class Main extends Settings {
                     ": Out of time! Let's go!");
 
             try {
-                methods.getResponse();
-//                    String answer = response.body().string();
-//                    BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Baza ofert\\CBOP_Output.txt"));
-//                    writer.write(answer);
-//                    writer.close();
+                methods.callAndExecuteRequest();
+                methods.downloadResponseFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
